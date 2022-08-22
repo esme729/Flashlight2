da45f5c03b7d507ddacc667804b8fef069ca2a6f
Esmay <Molinaesme729@gmail.com>
2022-08-22 09:26:42 -0600

    Initial Commit

---
+++ Flashlight2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashlight2
+//
+//  Created by Molina, Yadira - Student on 8/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
